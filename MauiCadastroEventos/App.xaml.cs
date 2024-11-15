7e6cec6fc73d5296127060f07612dea28b202b9c
Gfiles <gfilesg@gmail.com>
2024-11-15 01:00:51 -0300

    Add project files.

---
+++ MauiCadastroEventos/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiCadastroEventos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
